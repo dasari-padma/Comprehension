5c18e645270dffa155132cb1373b4d110333a824
dasari-padma <110389750+dasari-padma@users.noreply.github.com>
2022-09-21 14:45:03 +0530

    Update ComprehensionApplicationTests.java

--- src/test/java/com/example/Doctor/ComprehensionApplicationTests.java
+++ src/test/java/com/example/Doctor/ComprehensionApplicationTests.java
@@ -1,13 +1,1 @@
-package com.example.Doctor;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ComprehensionApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
